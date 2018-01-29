f803f23887a855d4456bb96b8388594e72b3c365
TriciaF <TriciaF@thinkful.com>
2018-01-29 14:30:38 -0500

    created redux actions for hotColdApp

---
+++ src/components/reducers/index.js
@@ -0,0 +1,19 @@
+import * as actions from '../actions';
+
+
+const initailState = {
+  guesses: [],
+  feedback: 'Make your guess!',
+  auralStatus: '',
+  correctAnswer: Math.round(Math.random() * 100) + 1
+};
+
+export const hotColdReducer = (state = initialState, action )=> {
+//place the actions
+make a guess
+start a new game
+generateAuralUpdate
+
+
+
+}
